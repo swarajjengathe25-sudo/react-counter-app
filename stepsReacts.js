7710c7ce3743f26f31cdc171d301e1ead4fd7883
Swaraj Jengathe <swarajjengathe25@gmail.com>
2026-06-02 15:17:12 +0530

    Remove unused logo import

---
+++ stepsReacts.js
@@ -0,0 +1,21 @@
+/*cd "C:\Users\Hp\Downloads\DOT\React\ReactStarterPack" */
+/*starter pack ke liye node ke - 
+--- npm init -y
+--- npm i express mongoose dotenv cors cookie-parser bcryptjs jsonwebtoken validator multer nodemailer
+ye agr project krna hai to kr sakte hai aur agr project ke liye file chahiye to 
+--- npx create-react-app reacts-basics (jis bhi nam se bnana chahte ho vo) ye ke starter pack hota hai isme files aa jati hai
+
+
+aur agr taliwind ki bhi file lani ho to 
+--- npm install -D tailwindcss
+fir
+--- npx tailwindcss init -p
+
+to talwind ki file aa jayegi 
+
+*/
+
+/*Icrement & Decrement project
+to sb code ye App.js file me aayega 
+
+agr UI pe ki bhi chanages update krna ho to useState() ka use krna hoga */
